30ca13cfb453a437378694491710baa317426471
Marco <scootermac1012@gmail.com>
2020-02-20 20:43:24 -0400

    creacion de tablas

---
+++ Introduccion/intro.sql
@@ -0,0 +1,11 @@
+/*Crear la tabla*/
+CREATE TABLE Alumno
+(Nombre char(50),
+Apellido char(50),);
+
+/*Agregar registros en la tabla*/
+INSERT INTO Alumno ("Nombre", "Apellido",)
+VALUES ("Marco", "Aguilar",);
+
+/*Mostrar datos de la tabla*/
+SELECT "Nombre" FROM Alumno;
